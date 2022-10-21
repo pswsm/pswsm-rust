7ccfacfad51e01e6cec72ddedf0c4956f80aaa65
Pau Figueras <pau@pswsm.cat>
2022-10-21 18:06:02 +0200

    [WIP] Query entitites

--- bevy_2d/src/entities.rs
+++ bevy_2d/src/entities.rs
@@ -13,10 +13,12 @@ pub struct Person {
 }
 
 pub fn make_people(mut cmds: Commands) -> () {
-    cmds.spawn_bundle(Person {a: Name("JL".to_string()), b: Age(47)});
+    cmds.spawn_bundle(Person {a: Name("Example".to_string()), b: Age(0)})
+        .insert_bundle(Person {a: Name("PF".to_string()), b: Age(19)})
+        .insert_bundle(Person {a: Name("JL".to_string()), b: Age(47)});
 }
 
-pub fn greet_people(query: Query<&Name, With<Age>>) -> () {
+pub fn greet_people(query: Query<&Name, &Age>) -> () {
     for name in query.iter() {
         println!("hello {}!", name.0);
     }
