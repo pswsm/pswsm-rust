6495861a8bef3be821c43b0b27f2530f87aa8ccb
Pau Figueras <pau@pswsm.cat>
2022-10-26 14:38:56 +0200

    Update camera conf

--- bevy_2d/src/camera.rs
+++ bevy_2d/src/camera.rs
@@ -1,11 +1,6 @@
 use bevy::prelude::*;
 
 pub fn spawn_camera(mut commands: Commands) {
-    let mut camera: Camera2dBundle = Camera2dBundle::default();
-    camera.projection.top = 1.0;
-    camera.projection.right = 1.0;
-    camera.projection.bottom = -1.0;
-    camera.projection.left = -1.0;
-
+    let camera: Camera2dBundle = Camera2dBundle::default();
     commands.spawn_bundle(camera);
 }
